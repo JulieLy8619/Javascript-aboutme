cefe88ccf2a26448aa4ce40fe2470cf57487cee2
Skyler Burger <skylerburger@gmail.com>
2018-09-15 16:11:43 -0700

    turned username question into a function

--- js/app.js
+++ js/app.js
@@ -2,114 +2,117 @@
 
 var totalAllQuestionsRight = 0;
 
-// //ask user name
-// var userName = prompt('Greetings and Salutations, what do you call yourself?');
-// alert("Nice to meet you, " + userName);
-// console.log('User\'s name: ' + userName)
+//ask user name
+var getName = function() {
 
-// //tell user I am quizzing them
-// alert("Let's play a game, a guessing game");
+    var userName = prompt('Greetings and Salutations, what do you call yourself?');
+    alert("Nice to meet you, " + userName);
+    console.log('User\'s name: ' + userName)
+}
+getName();
+//tell user I am quizzing them
+alert("Let's play a game, a guessing game");
 
-// //ask Gender question, answer: No
-// var genderAnswer = prompt('Am I a male?');
+//ask Gender question, answer: No
+var genderAnswer = prompt('Am I a male?');
 
-// //from class review, it is suggested to assign the .toUpperCase to a varialble to use that in the if statement so the code looks cleaner
-// if (genderAnswer.toUpperCase() === 'YES' || genderAnswer.toUpperCase() === 'Y') {
-//     alert('Good try, but I am a female');
-// } else if (genderAnswer.toUpperCase() === "NO" || genderAnswer.toUpperCase() === 'N') {
-//     alert('Good job, I am a female');
-//     totalAllQuestionsRight++;
-//     console.log('Q1 ' + totalAllQuestionsRight);
-// } else {
-//     alert('That was not a "yes" or "no" answer');
-// }
-// console.log('User\'s answer to if I am a male: ' + genderAnswer)
+//from class review, it is suggested to assign the .toUpperCase to a varialble to use that in the if statement so the code looks cleaner
+if (genderAnswer.toUpperCase() === 'YES' || genderAnswer.toUpperCase() === 'Y') {
+    alert('Good try, but I am a female');
+} else if (genderAnswer.toUpperCase() === "NO" || genderAnswer.toUpperCase() === 'N') {
+    alert('Good job, I am a female');
+    totalAllQuestionsRight++;
+    console.log('Q1 ' + totalAllQuestionsRight);
+} else {
+    alert('That was not a "yes" or "no" answer');
+}
+console.log('User\'s answer to if I am a male: ' + genderAnswer)
 
-// //ask food question, answer: Yes
-// var foodAnswer = prompt('Do I love chinese food?');
-// if (foodAnswer.toUpperCase() === 'YES' || foodAnswer.toUpperCase() === 'Y') {
-//     alert('Yes, I LOOOOOVE Chinese food');
-//     totalAllQuestionsRight++;
-//     console.log('Q2 ' + totalAllQuestionsRight);
-// } else if (foodAnswer.toUpperCase() === "NO" || foodAnswer.toUpperCase() === 'N') {
-//     alert('Good try, maybe I can introduce you to some delicious chinese food');
-// } else {
-//     alert('That was not a "yes" or "no" answer');
-// }
-// console.log('User\'s answer to if I love Chinese food: ' + foodAnswer)
+//ask food question, answer: Yes
+var foodAnswer = prompt('Do I love chinese food?');
+if (foodAnswer.toUpperCase() === 'YES' || foodAnswer.toUpperCase() === 'Y') {
+    alert('Yes, I LOOOOOVE Chinese food');
+    totalAllQuestionsRight++;
+    console.log('Q2 ' + totalAllQuestionsRight);
+} else if (foodAnswer.toUpperCase() === "NO" || foodAnswer.toUpperCase() === 'N') {
+    alert('Good try, maybe I can introduce you to some delicious chinese food');
+} else {
+    alert('That was not a "yes" or "no" answer');
+}
+console.log('User\'s answer to if I love Chinese food: ' + foodAnswer)
 
-// //ask 301 class question, answer: No
-// var classAnswer = prompt('Am I taking Code Fellows 301?');
-// if (classAnswer.toUpperCase() === 'YES' || classAnswer.toUpperCase() === 'Y') {
-//     alert('Good try, but we are still in 201');
-// } else if (classAnswer.toUpperCase() === "NO" || classAnswer.toUpperCase() === 'N') {
-//     alert('Good job, we are in 201');
-//     totalAllQuestionsRight++;
-//     console.log('Q3 ' + totalAllQuestionsRight);
-// } else {
-//     alert('That was not a "yes" or "no" answer');
-// }
-// console.log('User\'s answer to 301 class: ' + classAnswer)
+//ask 301 class question, answer: No
+var classAnswer = prompt('Am I taking Code Fellows 301?');
+if (classAnswer.toUpperCase() === 'YES' || classAnswer.toUpperCase() === 'Y') {
+    alert('Good try, but we are still in 201');
+} else if (classAnswer.toUpperCase() === "NO" || classAnswer.toUpperCase() === 'N') {
+    alert('Good job, we are in 201');
+    totalAllQuestionsRight++;
+    console.log('Q3 ' + totalAllQuestionsRight);
+} else {
+    alert('That was not a "yes" or "no" answer');
+}
+console.log('User\'s answer to 301 class: ' + classAnswer)
 
-// //ask python questions, answer: Yes
-// var pythonAnswer = prompt('Do I want to take Python');
-// if (pythonAnswer.toUpperCase() === 'YES' || pythonAnswer.toUpperCase() === 'Y') {
-//     alert('Good job, I do plan on Python 401 at Code Fellows');
-//     totalAllQuestionsRight++;
-//     console.log('Q4 ' + totalAllQuestionsRight);
-// } else if (pythonAnswer.toUpperCase() === "NO" || pythonAnswer.toUpperCase() === 'N') {
-//     alert('Good try, but I do plan on Python 401 at Code Fellows');
-// } else {
-//     alert('That was not a "yes" or "no" answer');
-// }
-// console.log('User\'s answer to python question: ' + pythonAnswer)
+//ask python questions, answer: Yes
+var pythonAnswer = prompt('Do I want to take Python');
+if (pythonAnswer.toUpperCase() === 'YES' || pythonAnswer.toUpperCase() === 'Y') {
+    alert('Good job, I do plan on Python 401 at Code Fellows');
+    totalAllQuestionsRight++;
+    console.log('Q4 ' + totalAllQuestionsRight);
+} else if (pythonAnswer.toUpperCase() === "NO" || pythonAnswer.toUpperCase() === 'N') {
+    alert('Good try, but I do plan on Python 401 at Code Fellows');
+} else {
+    alert('That was not a "yes" or "no" answer');
+}
+console.log('User\'s answer to python question: ' + pythonAnswer)
 
-// //ask amazing question, but its not really a question, there is only one right answer ;), answer: yes
-// var amazingAnswer = prompt('Am I AMAZING!?!?!?!?');
-// if (amazingAnswer.toUpperCase() === 'YES' || amazingAnswer.toUpperCase() === 'Y') {
-//     alert('Great job, I knew I was amazing and glad others notice too \;\)');
-//     totalAllQuestionsRight++;
-//     console.log('Q5 ' + totalAllQuestionsRight);
-// } else if (amazingAnswer.toUpperCase() === "NO" || amazingAnswer.toUpperCase() === 'N') {
-//     alert('Clearly you don\'t know me well enough yet, because I am AMAZING');
-// } else {
-//     alert('That was not a "yes" or "no" answer');
-// }
-// console.log('User\'s answer to if i am amazing: ' + amazingAnswer)
+//ask amazing question, but its not really a question, there is only one right answer ;), answer: yes
+var amazingAnswer = prompt('Am I AMAZING!?!?!?!?');
+if (amazingAnswer.toUpperCase() === 'YES' || amazingAnswer.toUpperCase() === 'Y') {
+    alert('Great job, I knew I was amazing and glad others notice too \;\)');
+    totalAllQuestionsRight++;
+    console.log('Q5 ' + totalAllQuestionsRight);
+} else if (amazingAnswer.toUpperCase() === "NO" || amazingAnswer.toUpperCase() === 'N') {
+    alert('Clearly you don\'t know me well enough yet, because I am AMAZING');
+} else {
+    alert('That was not a "yes" or "no" answer');
+}
+console.log('User\'s answer to if i am amazing: ' + amazingAnswer)
 
-// //question 6, guess a number and play too high too low up to 4 times
-// var countGuesses = 4;
-// var guessedRight = false;
+//question 6, guess a number and play too high too low up to 4 times
+var countGuesses = 4;
+var guessedRight = false;
 
-// while (countGuesses > 0) {
-//     var userGuess = prompt('What is my FAVORITE number? Note: ' + countGuesses + ' guesses left');
+while (countGuesses > 0) {
+    var userGuess = prompt('What is my FAVORITE number? Note: ' + countGuesses + ' guesses left');
 
-//     //this is to check there was a guess even done
-//     while (!userGuess){
-//         var userGuess = prompt('You need to make a guess of SOMETHING...');
-//     }
+    //this is to check there was a guess even done
+    while (!userGuess){
+        var userGuess = prompt('You need to make a guess of SOMETHING...');
+    }
 
-//     if (userGuess == 36) {
-//         alert ('Ta-D!a Confetti! Balloons! You got it, 36 is my favorite number');
-//         countGuesses = -10; //so i escape the loop
-//         guessedRight = true; //so i know if I need to tell them the right answer later or not
-//         totalAllQuestionsRight++;
-//         console.log('Q6 ' + totalAllQuestionsRight);
-//     } else if (userGuess > 36) {
-//         alert ('Good guess, but my favorite number is lower');
-//     } else if (userGuess < 36) {
-//         alert ('Good guess, but my favorite number is higher');
-//     } else {
-//         alert ('You were supposed to guess a number');
-//     }
-//     countGuesses--;
-//     console.log('users guess ' + userGuess)
-//     console.log('count guesses ' + countGuesses)
-// }
+    if (userGuess == 36) {
+        alert ('Ta-D!a Confetti! Balloons! You got it, 36 is my favorite number');
+        countGuesses = -10; //so i escape the loop
+        guessedRight = true; //so i know if I need to tell them the right answer later or not
+        totalAllQuestionsRight++;
+        console.log('Q6 ' + totalAllQuestionsRight);
+    } else if (userGuess > 36) {
+        alert ('Good guess, but my favorite number is lower');
+    } else if (userGuess < 36) {
+        alert ('Good guess, but my favorite number is higher');
+    } else {
+        alert ('You were supposed to guess a number');
+    }
+    countGuesses--;
+    console.log('users guess ' + userGuess)
+    console.log('count guesses ' + countGuesses)
+}
 
-// if (guessedRight==false){
-//     alert('Good effort, my favorite number is 36');
-// }
+if (guessedRight==false){
+    alert('Good effort, my favorite number is 36');
+}
 
 //question 7, has multiple answers, up to 6 guesses, and a comment they got # of # right
 var countGuessesQ7 = 6;
